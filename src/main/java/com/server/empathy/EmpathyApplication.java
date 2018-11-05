88232ae8f817dd15fa3d3ca3034cdccd8d43d2d3
onemoon <onemoonstudio@gmail.com>
2018-11-05 17:51:36 +0900

    Feat : Spring boot Base
    Feat  Spring Boot Base

---
+++ src/main/java/com/server/empathy/EmpathyApplication.java
@@ -0,0 +1,12 @@
+package com.server.empathy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpathyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpathyApplication.class, args);
+    }
+}
