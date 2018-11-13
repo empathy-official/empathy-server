f5e6ccc29df36f5374144cc14a8104968c472c14
onemoon <onemoonstudio@gmail.com>
2018-11-13 14:57:19 +0900

    Refactor : Base

--- src/main/java/com/server/empathy/EmpathyApplication.java
+++ src/main/java/com/server/empathy/EmpathyApplication.java
@@ -1,23 +1,12 @@
 package com.server.empathy;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class EmpathyApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.properties,"
-            + "classpath:application-dev.properties";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EmpathyApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
-
-
-//        SpringApplication.run(EmpathyApplication.class, args);
+        SpringApplication.run(EmpathyApplication.class, args);
     }
 }
