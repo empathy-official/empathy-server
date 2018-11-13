e4e435859eb1729d657b15a5aa4b0af7eb56583c
onemoon <onemoonstudio@gmail.com>
2018-11-13 12:57:50 +0900

    Feat & Refactor : Filter API
    Feat
    Filter Type API
    
    Refactor
    Filter API

---
+++ src/main/java/com/server/empathy/repository/FilterTypeRepository.java
@@ -0,0 +1,8 @@
+package com.server.empathy.repository;
+
+import com.server.empathy.entity.FilterType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FilterTypeRepository extends CrudRepository<FilterType, Long> {
+
+}
